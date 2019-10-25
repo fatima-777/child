352b78db608b98df2561f720fcfd5ce2d9ae1179
fatima-777 <fzehra777@gmail.com>
2019-10-25 21:35:46 +0500

    child

---
+++ wordpress/wp-content/themes/Divi-Child/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.row').slick({
+      'infinite': 'true',
+      'slidesToShow': 3,
+      'slidesToScroll': 3,
+      'arrows': 'true',
+      'autoplay': 'true',
+    })});
